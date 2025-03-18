32bea462decaacd28aadd07ac0f9678f9d581cfe
bb305496 <bartekbielak12@onet.pl>
2025-03-18 17:59:41 +0100

    Add campfire and BGMusic Sound

---
+++ Assets/Main_menu/Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("-----------Audio Source-----------")]
+    [SerializeField] AudioSource bgMusicSource;
+    [SerializeField] AudioSource campfireMusicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("-----------Audio Clip-----------")]
+    public AudioClip buttonSound;
+
+    //TO DO add buttonSound() function
+
+}
