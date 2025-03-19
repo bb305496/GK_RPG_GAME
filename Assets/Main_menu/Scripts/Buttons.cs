dac72a63da2e33f7983a2ed496bcb2d0aaf7e35d
bb305496 <bartekbielak12@onet.pl>
2025-03-19 09:08:51 +0100

    DelayOnButtons
    BuxFix - Animations and sound are on time

--- Assets/Main_menu/Scripts/Buttons.cs
+++ Assets/Main_menu/Scripts/Buttons.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,16 +6,28 @@ public class ButtonsScript : MonoBehaviour
 {
     public void PlayButtonClick()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(DelayBeforeSceneChange(1));
     }
 
     public void OptionsButtonClick()
     {
-        SceneManager.LoadScene(2);
+        StartCoroutine(DelayBeforeSceneChange(2));
     }
 
     public void ExitButtonClick()
     {
+        StartCoroutine(DelayBeforeExit());
+    }
+ 
+    private IEnumerator DelayBeforeSceneChange(int sceneInxed)
+    {
+        yield return new WaitForSeconds(0.7f);
+        SceneManager.LoadScene(sceneInxed);
+    }
+
+    private IEnumerator DelayBeforeExit()
+    {
+        yield return new WaitForSeconds(0.7f);
         Debug.Log("Exit");
         Application.Quit();
     }
