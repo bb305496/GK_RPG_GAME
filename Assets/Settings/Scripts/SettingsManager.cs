4e6c4ad72ac68c31cd266f5502e7e1953a9a1069
bb305496 <bartekbielak12@onet.pl>
2025-03-19 11:49:26 +0100

    Add GraphicsSettings

--- Assets/Settings/Scripts/SettingsManager.cs
+++ Assets/Settings/Scripts/SettingsManager.cs
@@ -4,5 +4,10 @@ using TMPro;
 
 public class SettingsManager : MonoBehaviour
 {
-    
+    public TMP_Dropdown graphicsDropdown;
+
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
 }
