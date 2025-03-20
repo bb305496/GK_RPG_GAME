3eac135e8dc577316bb2d81b2e0f2df1790feaf6
bb305496 <bartekbielak12@onet.pl>
2025-03-20 11:07:26 +0100

    Add PlayerMovementScript

--- Assets/MainCharacter/Scripts/PlayerMovement.cs
+++ Assets/MainCharacter/Scripts/PlayerMovement.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public Rigidbody2D rb;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2 (horizontal, vertical) * speed;
     }
 }
