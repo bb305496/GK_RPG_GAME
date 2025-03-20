7516fc2d6720f2b1d56f2ea757fd21bbef05a7e4
bb305496 <bartekbielak12@onet.pl>
2025-03-20 09:37:15 +0100

    ButtonsSoundBugFix

--- Assets/Main_menu/Scripts/AudioManager.cs
+++ Assets/Main_menu/Scripts/AudioManager.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
 
     [Header("-----------Audio Source-----------")]
     [SerializeField] AudioSource bgMusicSource;
     [SerializeField] AudioSource campfireMusicSource;
-    [SerializeField] AudioSource buttonClick;
 
     private void Start()
     {
         bgMusicSource.Play();
         campfireMusicSource.Play();
-    }
-
-    public void PlayButtonSound()
-    {
-        buttonClick.Play();
     }
 }
