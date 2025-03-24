a412544733499c56dcae19f865722a21473c1db0
bb305496 <bartekbielak12@onet.pl>
2025-03-24 09:29:38 +0100

    Add EnemyFacingDirection

--- Assets/Enemy/BasicDemo/Scripts/EnemyMovement.cs
+++ Assets/Enemy/BasicDemo/Scripts/EnemyMovement.cs
@@ -1,9 +1,11 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Transform player;
+    private int facingDirection = 1;
 
     public float speed = 1f;
     private bool isChasing;
@@ -17,9 +19,20 @@ public class EnemyMovement : MonoBehaviour
     {
         if (isChasing == true)
         {
+            if (player.position.x > transform.position.x && facingDirection == -1 ||
+               player.position.x < transform.position.x && facingDirection == 1)
+            {
+                Flip();
+            }
             Vector2 direction = (player.position - transform.position).normalized;
             rb.linearVelocity = direction * speed;
         }
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
