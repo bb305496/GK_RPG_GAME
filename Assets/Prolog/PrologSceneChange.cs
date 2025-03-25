22c8b0587d8cf8db9fdb7717763fe34bc63c3e09
bb305496 <bartekbielak12@onet.pl>
2025-03-25 18:14:18 +0100

    Add PrologSkip

---
+++ Assets/Prolog/PrologSceneChange.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrologSceneChange : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(DelayBeforeSceneChange(82f, 4));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+
+    private IEnumerator DelayBeforeSceneChange(float seconds, int sceneInxed)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(sceneInxed);
+    }
+}
