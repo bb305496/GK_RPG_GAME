3db466fe572ffc031ea560c959253ddad5161832
bb305496 <bartekbielak12@onet.pl>
2025-03-26 13:44:34 +0100

    Fix Enemy atacking

--- Assets/Enemy/BasicDemo/Scripts/EnemyMovement.cs
+++ Assets/Enemy/BasicDemo/Scripts/EnemyMovement.cs
@@ -55,7 +55,7 @@ public class EnemyMovement : MonoBehaviour
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
