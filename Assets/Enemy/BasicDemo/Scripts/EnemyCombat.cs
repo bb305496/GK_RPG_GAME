b38fcb524dede77df1acafac9e96c1e67b20c7c4
bb305496 <bartekbielak12@onet.pl>
2025-03-26 13:56:23 +0100

    Fix attacking

--- Assets/Enemy/BasicDemo/Scripts/EnemyCombat.cs
+++ Assets/Enemy/BasicDemo/Scripts/EnemyCombat.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class EnemyCombat : MonoBehaviour
 {
     public int damage = 1;
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
-        }
-    }
+    public Transform attackPoint;
+    public float weaponRange;
+    public LayerMask playerLayer;
 
     public void Attack()
     {
-        Debug.Log("Atttacking");
+        Collider2D[] hits = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, playerLayer);
+
+        if(hits.Length > 0)
+        {
+            hits[0].GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        }
     }
 }
