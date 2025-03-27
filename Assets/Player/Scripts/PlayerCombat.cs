6d26b08bbf55162cc68e6cd48494d145522059d7
bb305496 <bartekbielak12@onet.pl>
2025-03-27 15:37:59 +0100

    Add HeavyAttack

--- Assets/Player/Scripts/PlayerCombat.cs
+++ Assets/Player/Scripts/PlayerCombat.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    public Transform attackPoint;
+    public Transform attackPoint1;
+    public Transform attackPoint2;
     public float weaponRange = 1f;
     public float knockbackForce = 5;
     public float knockbackTime = 0.15f;
@@ -16,11 +17,19 @@ public class PlayerCombat : MonoBehaviour
     public float attack1Cooldown = 1f;
     private float attack1CooldownTimer;
 
+    public float attack2Cooldown = 2f;
+    private float attack2CooldownTimer;
+
     private void Update()
     {
         if(attack1CooldownTimer > 0)
         {
             attack1CooldownTimer -= Time.deltaTime;
+        }
+
+        if (attack2CooldownTimer > 0)
+        {
+            attack2CooldownTimer -= Time.deltaTime;
         }
     }
 
@@ -33,9 +42,18 @@ public class PlayerCombat : MonoBehaviour
         }
     }
 
-    public void DealDamage()
+    public void Attack2()
     {
-        Collider2D[] enemis = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+        if (attack2CooldownTimer <= 0)
+        {
+            anim.SetBool("isAttacking2", true);
+            attack2CooldownTimer = attack2Cooldown;
+        }
+    }
+
+    public void DealDamageAttack1()
+    {
+        Collider2D[] enemis = Physics2D.OverlapCircleAll(attackPoint1.position, weaponRange, enemyLayer);
 
         if (enemis.Length > 0)
         {
@@ -44,15 +62,34 @@ public class PlayerCombat : MonoBehaviour
         }
     }
 
+    public void DealDamageAttack2()
+    {
+        Collider2D[] enemis = Physics2D.OverlapCircleAll(attackPoint2.position, weaponRange, enemyLayer);
+
+        if (enemis.Length > 0)
+        {
+            enemis[0].GetComponent<EnemyHealth>().ChangeHealth(-(2*damage));
+            enemis[0].GetComponent<EnemyKnockback>().Knockback(transform, 1.2f*knockbackForce, 1.2f*knockbackTime, 1.2f*stunTime);
+        }
+    }
+
     public void FinishAttacking1()
     {
         anim.SetBool("isAttacking1", false);
     }
 
+    public void FinishAttacking2()
+    {
+        anim.SetBool("isAttacking2", false);
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
+        Gizmos.DrawWireSphere(attackPoint1.position, weaponRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint2.position, weaponRange);
 
     }
 }
