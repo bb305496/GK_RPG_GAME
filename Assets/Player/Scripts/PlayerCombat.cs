19fbc4bc6a284c45fb3d400afa4ff7f049c63597
bb305496 <bartekbielak12@onet.pl>
2025-03-27 14:04:31 +0100

    Add Damaging enemy

--- Assets/Player/Scripts/PlayerCombat.cs
+++ Assets/Player/Scripts/PlayerCombat.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    public Transform attackPoint;
+    public float weaponRange = 1f;
+    public LayerMask enemyLayer;
+    public int damage = 1;
+
+
     public Animator anim;
     public float attack1Cooldown = 1f;
     private float attack1CooldownTimer;
@@ -23,8 +29,25 @@ public class PlayerCombat : MonoBehaviour
         }
     }
 
+    public void DealDamage()
+    {
+        Collider2D[] enemis = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+
+        if (enemis.Length > 0)
+        {
+            enemis[0].GetComponent<EnemyHealth>().ChangeHealth(-damage);
+        }
+    }
+
     public void FinishAttacking1()
     {
         anim.SetBool("isAttacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
+
     }
 }
