74b7b1c07b72f8975c7ed9df92c70c420695f05a
KrzysztofGra <krzygra357@student.polsl.pl>
2025-04-03 21:15:31 +0200

    add chasingWarning

---
+++ Assets/Player/Scripts/ChasingWarning.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System;
+using TMPro;
+
+public class ChasingWarningUI : MonoBehaviour
+{
+    public GameObject warningUI; // np. Image z Canvas
+    private EnemyMovement[] enemies;
+    private float warningTimer = 0f;
+    public float warningDuration = 2f;
+    public GameObject hpGlowAnimator; // lub Image / CanvasGroup jeœli wolisz inny typ
+
+
+
+
+    void Start()
+    {
+        enemies = UnityEngine.Object.FindObjectsByType<EnemyMovement>(FindObjectsSortMode.None);
+
+        UnityEngine.Debug.Log($"Znaleziono przeciwników: {enemies.Length}");
+        warningUI.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        enemies = UnityEngine.Object.FindObjectsByType<EnemyMovement>(FindObjectsSortMode.None);
+
+        bool isChased = false;
+
+        foreach (EnemyMovement enemy in enemies)
+        {
+            if (enemy.CurrentState == EnemyState.Chasing || enemy.CurrentState == EnemyState.Attacking)
+            {
+                isChased = true;
+                break;
+            }
+        }
+
+        if (isChased)
+        {
+            warningTimer = warningDuration; // resetuj licznik
+        }
+        else
+        {
+            warningTimer -= Time.deltaTime;
+        }
+
+        if (warningTimer > 0)
+        {
+            if (!hpGlowAnimator.gameObject.activeSelf)
+                hpGlowAnimator.gameObject.SetActive(true);
+        }
+        else
+        {
+            hpGlowAnimator.gameObject.SetActive(false);
+        }
+
+    }
+
+    public bool IsBeingChased()
+    {
+        return warningTimer > 0;
+    }
+
+
+}
