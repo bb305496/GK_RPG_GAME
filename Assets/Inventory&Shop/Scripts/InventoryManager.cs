713390af401588511c0d23504fe942d698710bb7
bb305496 <bartekbielak12@onet.pl>
2025-04-04 13:50:37 +0200

    Add GoldCollecting

---
+++ Assets/Inventory&Shop/Scripts/InventoryManager.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public int gold;
+    public TMP_Text goldText;
+
+    private void OnEnable()
+    {
+        Loot.OnItemLooted += AddItem;
+    }
+
+    private void OnDisable()
+    {
+        Loot.OnItemLooted -= AddItem;
+    }
+
+    public void AddItem(ItemSO itemSO, int quantity)
+    {
+        if(itemSO.isGold)
+        {
+            gold += quantity;
+            goldText.text = gold.ToString();
+            return;
+        }
+    }
+}
--- Assets/Inventory&Shop/Scripts/Loot.cs
+++ Assets/Inventory&Shop/Scripts/Loot.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Loot : MonoBehaviour
@@ -7,6 +8,7 @@ public class Loot : MonoBehaviour
     public Animator anim;
 
     public int quantity;
+    public static event Action<ItemSO, int> OnItemLooted;
 
     private void OnValidate()
     {
@@ -22,6 +24,7 @@ public class Loot : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             anim.Play("LootPickUp");
+            OnItemLooted?.Invoke(itemSO, quantity);
             Destroy(gameObject, 0.5f);
         }
     }
