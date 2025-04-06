23c95094419fffce8b9fc0f4c8e7b9b211f08482
bb305496 <bartekbielak12@onet.pl>
2025-04-06 19:10:30 +0200

    PopulateShopItems

---
+++ Assets/Inventory&Shop/Scripts/Shop/ShopSlot.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSlot : MonoBehaviour
+{
+    public ItemSO itemSO;
+    public TMP_Text itemNameText;
+    public TMP_Text itemPriceText;
+    public Image itemImage;
+
+    private int price;
+
+    public void Initialize(ItemSO newItemSO, int price)
+    {
+        itemSO = newItemSO;
+        itemImage.sprite = itemSO.icon;
+        itemNameText.text = itemSO.itemName;
+        this.price = price;
+        itemPriceText.text = price.ToString();
+    }
+}
