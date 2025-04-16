422bcf7f38ae6de5c4a66e58e4e8a0c397291058
bb305496 <bartekbielak12@onet.pl>
2025-04-16 14:11:47 +0200

    Add Wander State

---
+++ Assets/NPC/Scripts/NPC_Wander.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class NPC_Wander : MonoBehaviour
+{
+    [Header("Wander Area")]
+    public float wanderWidth = 5f;
+    public float wanderHeight = 5f;
+    public Vector2 startingPosition;
+
+    public float pauseDuration = 1f;
+    public float speed = 2f;
+    public Vector2 target;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(PauseAndPickNewDirection());
+    }
+
+    public void Update()
+    {
+        if (isPaused)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
+        if(Vector2.Distance(transform.position, target) < 0.1f)
+        {
+            StartCoroutine(PauseAndPickNewDirection());
+        }
+
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+        if(direction.x > 0 && transform.localScale.x < 0 || direction.x < 0 && transform.localScale.x > 0)
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+
+        rb.linearVelocity = direction * speed;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(startingPosition, new Vector3(wanderWidth, wanderHeight, 0));
+    }
+
+    IEnumerator PauseAndPickNewDirection()
+    {
+        isPaused = true;
+        anim.Play("Idle");
+        yield return new WaitForSeconds(pauseDuration);
+
+        target = GetRandomTarget();
+        isPaused = false;
+        anim.Play("Walk");
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+            StartCoroutine(PauseAndPickNewDirection());
+    }
+
+    private Vector2 GetRandomTarget()
+    {
+        float halfwidth = wanderWidth / 2;
+        float halfheight = wanderHeight / 2;
+        int edge = Random.Range(0, 4);
+
+        return edge switch
+        {
+            0 => new Vector2(startingPosition.x - halfwidth, Random.Range(startingPosition.y - halfheight, startingPosition.y + halfheight)),
+            1 => new Vector2(startingPosition.x + halfwidth, Random.Range(startingPosition.y - halfheight, startingPosition.y + halfheight)),
+            2 => new Vector2(Random.Range(startingPosition.x - halfwidth, startingPosition.x + halfwidth), startingPosition.y - halfheight),
+            _ => new Vector2(Random.Range(startingPosition.x - halfwidth, startingPosition.x + halfwidth), startingPosition.y + halfheight)
+        };
+    }
+}
