88f447a862a4e9c2b8036451dfeed50ccf1e26e7
bb305496 <bartekbielak12@onet.pl>
2025-04-22 14:53:30 +0200

    Correct Scene change

--- Assets/SceneChange/Scripts/SceneChanger.cs
+++ Assets/SceneChange/Scripts/SceneChanger.cs
@@ -7,11 +7,14 @@ public class SceneChanger : MonoBehaviour
     public string SceneToLoad;
     public Animator fadeAnim;
     public float fadeTime = 1f;
+    public Vector2 newPlayerPosition;
+    private Transform player;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            player = collision.transform;
             fadeAnim.Play("FadeToBlack");
             StartCoroutine(DelayFade());
         }
@@ -20,6 +23,7 @@ public class SceneChanger : MonoBehaviour
     IEnumerator DelayFade()
     {
         yield return new WaitForSeconds(fadeTime);
+        player.position = newPlayerPosition;
         SceneManager.LoadScene(SceneToLoad);
     }
 }
