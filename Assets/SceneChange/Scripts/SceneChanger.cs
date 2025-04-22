0fe9ad53180b7e94456110089ad6f935aef782f9
bb305496 <bartekbielak12@onet.pl>
2025-04-22 13:35:02 +0200

    Scene FadeAnimations

--- Assets/SceneChange/Scripts/SceneChanger.cs
+++ Assets/SceneChange/Scripts/SceneChanger.cs
@@ -1,15 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     public string SceneToLoad;
+    public Animator fadeAnim;
+    public float fadeTime = 1f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneToLoad);
+            fadeAnim.Play("FadeToBlack");
+            StartCoroutine(DelayFade());
         }
+    }
+
+    IEnumerator DelayFade()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(SceneToLoad);
     }
 }
