52dec1233f865b5a12ed302a020d431cf583ff79
bb305496 <bartekbielak12@onet.pl>
2025-04-23 17:35:25 +0200

    Add teleport

--- Assets/SceneChange/Scripts/SceneChanger.cs
+++ Assets/SceneChange/Scripts/SceneChanger.cs
@@ -1,6 +1,9 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneChanger : MonoBehaviour
 {
@@ -8,15 +11,56 @@ public class SceneChanger : MonoBehaviour
     public Animator fadeAnim;
     public float fadeTime = 1f;
     public Vector2 newPlayerPosition;
+    public Image levelReqimage;
+    public TMP_Text levelReqText;
     private Transform player;
+
+    private void Start()
+    {
+        if (levelReqimage != null)
+        {
+            levelReqimage.enabled = false;
+        }
+        if (levelReqText != null)
+        {
+            levelReqText.enabled = false;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && StatsManager.Instance.level >= 10)
         {
             player = collision.transform;
             fadeAnim.Play("FadeToBlack");
             StartCoroutine(DelayFade());
+        }
+        else
+        {
+            if(levelReqimage != null)
+            {
+                levelReqimage.enabled = true;
+            }
+            if (levelReqText != null)
+            {
+                levelReqText.enabled = true;
+            }
+            Debug.Log("You need to be level 10 to enter this area.");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (levelReqimage != null)
+            {
+                levelReqimage.enabled = false;
+            }
+            if (levelReqText != null)
+            {
+                levelReqText.enabled = false;
+            }
         }
     }
 
