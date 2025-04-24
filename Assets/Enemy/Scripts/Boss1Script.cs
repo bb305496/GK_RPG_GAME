d13e0a51a0d764b259a46a26ed120f9437becddc
bb305496 <bartekbielak12@onet.pl>
2025-04-24 11:09:58 +0200

    Boss1 basic script

---
+++ Assets/Enemy/Scripts/Boss1Script.cs
@@ -0,0 +1,133 @@
+using System.Diagnostics;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Boss1Script : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Transform player;
+    private Animator anim;
+    private EnemyState enemyState;
+    public float playerDetectRange = 5f;
+    public Transform detectionPoint;
+    public LayerMask playerLayer;
+
+    public float weight = 1f;
+    public float attackRange = 2f;
+    public float attackCooldown = 2f;
+    public float attackCooldownTimer;
+    private int facingDirection = -1;
+    public float speed = 1f;
+
+    public EnemyState CurrentState => enemyState;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
+    }
+
+    private void Update()
+    {
+        if (enemyState != EnemyState.TakingDamage)
+        {
+            CheckForPlayer();
+
+            if (attackCooldownTimer > 0)
+            {
+                attackCooldownTimer -= Time.deltaTime;
+            }
+            if (enemyState == EnemyState.Chasing)
+            {
+                Chase();
+            }
+            else if (enemyState == EnemyState.Attacking)
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+    }
+
+    public void Chase()
+    {
+
+        if (player.position.x > transform.position.x && facingDirection == -1 ||
+            player.position.x < transform.position.x && facingDirection == 1)
+        {
+            Flip();
+        }
+
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.linearVelocity = direction * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void CheckForPlayer()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectRange, playerLayer);
+
+        if (hits.Length > 0)
+        {
+            player = hits[0].transform;
+
+            if (Vector2.Distance(transform.position, player.transform.position) <= attackRange && attackCooldownTimer <= 0)
+            {
+                attackCooldownTimer = attackCooldown;
+                ChangeState(EnemyState.Attacking);
+            }
+            else if (Vector2.Distance(transform.position, player.position) > attackRange && enemyState != EnemyState.Attacking)
+            {
+                ChangeState(EnemyState.Chasing);
+            }
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+        }
+
+    }
+
+    public void ChangeState(EnemyState newState)
+    {
+        //Exit current animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", false);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isMoving", false);
+        else if (enemyState == EnemyState.Attacking)
+            anim.SetBool("isAttacking", false);
+        else if (enemyState == EnemyState.TakingDamage)
+            anim.SetBool("isTakingDamage", false);
+
+        //Update our current state
+        enemyState = newState;
+
+        //Update new animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", true);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isMoving", true);
+        else if (enemyState == EnemyState.Attacking)
+            anim.SetBool("isAttacking", true);
+        else if (enemyState == EnemyState.TakingDamage)
+            anim.SetBool("isTakingDamage", true);
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(detectionPoint.position, playerDetectRange);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(detectionPoint.position, attackRange);
+    }
+}
